a829d3b391ff58a0ef35a8a55bc297ace05ec831
qbit-0 <duypham12241999@gmail.com>
2021-12-16 16:45:18 -0800

    got it to teleport a bot

--- triton-soccer-ai/src/main/java/com/triton/module/SimulatorCommandCreator.java
+++ triton-soccer-ai/src/main/java/com/triton/module/SimulatorCommandCreator.java
@@ -1,9 +1,13 @@
 package com.triton.module;
 
+import proto.simulation.SslSimulationControl.TeleportRobot;
+
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import static com.triton.publisher_consumer.Exchange.SIMULATOR_COMMAND;
+import static proto.simulation.SslGcCommon.RobotId;
+import static proto.simulation.SslGcCommon.Team;
 import static proto.simulation.SslSimulationConfig.SimulatorConfig;
 import static proto.simulation.SslSimulationControl.SimulatorCommand;
 import static proto.simulation.SslSimulationControl.SimulatorControl;
@@ -28,6 +32,15 @@ public class SimulatorCommandCreator extends Module {
             SimulatorCommand.Builder command = SimulatorCommand.newBuilder();
 
             SimulatorControl.Builder control = SimulatorControl.newBuilder();
+            TeleportRobot.Builder teleportBot = TeleportRobot.newBuilder();
+            teleportBot.setId(RobotId.newBuilder().setId(0).setTeam(Team.BLUE));
+            teleportBot.setX(0);
+            teleportBot.setY(0);
+            teleportBot.setOrientation(0);
+            teleportBot.setVX(0);
+            teleportBot.setVY(0);
+            teleportBot.setVAngular(0);
+            control.addTeleportRobot(teleportBot);
             command.setControl(control);
 
             SimulatorConfig.Builder config = SimulatorConfig.newBuilder();
