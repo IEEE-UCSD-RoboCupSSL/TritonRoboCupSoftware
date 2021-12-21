dce5b6f4b5f71b2054ed827615e462064cc4d8fe
qbit-0 <duypham12241999@gmail.com>
2021-12-20 17:56:50 -0800

    rename config folder into config_yaml

--- triton-soccer-ai/src/main/java/com/triton/config/Config.java
+++ triton-soccer-ai/src/main/java/com/triton/config/Config.java
@@ -1,9 +1,9 @@
 package com.triton.config;
 
 public enum Config {
-    NETWORK_CONFIG("../config/network_config.yaml", NetworkConfig.class),
-    OBJECT_CONFIG("../config/object_config.yaml", ObjectConfig.class),
-    DISPLAY_CONFIG("../config/display_config.yaml", DisplayConfig.class),
+    NETWORK_CONFIG("../config_yaml/network_config.yaml", NetworkConfig.class),
+    OBJECT_CONFIG("../config_yaml/object_config.yaml", ObjectConfig.class),
+    DISPLAY_CONFIG("../config_yaml/display_config.yaml", DisplayConfig.class),
     ;
 
     private final String configPath;
