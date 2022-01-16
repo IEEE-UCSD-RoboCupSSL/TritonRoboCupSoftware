1e360b022571407260b462b4ff22bb520dae954f
qbit-0 <duypham12241999@gmail.com>
2022-01-15 20:14:09 -0800

    Started making the AI modules.

---
+++ triton-soccer-ai/src/main/java/com/triton/module/processing_module/ai_module/TacticsModule.java
@@ -0,0 +1,17 @@
+package com.triton.module.processing_module.ai_module;
+
+import com.triton.module.Module;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class TacticsModule extends Module {
+    public TacticsModule() throws IOException, TimeoutException {
+        super();
+    }
+
+    @Override
+    protected void declareExchanges() throws IOException, TimeoutException {
+        super.declareExchanges();
+    }
+}
