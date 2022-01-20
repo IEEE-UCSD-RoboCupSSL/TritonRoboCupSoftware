5d3c01bd2f5d644beeffc0c1e275847253e45072
qbit-0 <duypham12241999@gmail.com>
2022-01-19 23:47:27 -0800

    add consumes for IndividualSkillsModule

--- triton-soccer-ai/src/main/java/com/triton/module/ai_module/IndividualSkillsModule.java
+++ triton-soccer-ai/src/main/java/com/triton/module/ai_module/IndividualSkillsModule.java
@@ -3,17 +3,27 @@ package com.triton.module.ai_module;
 import com.rabbitmq.client.Delivery;
 import com.triton.ai.skills.individual_skills.*;
 import com.triton.module.Module;
+import proto.vision.MessagesRobocupSslDetection;
+import proto.vision.MessagesRobocupSslGeometry;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 
-import static com.triton.messaging.Exchange.AI_BASIC_SKILL;
-import static com.triton.messaging.Exchange.AI_INDIVIDUAL_SKILL;
+import static com.triton.messaging.Exchange.*;
 import static com.triton.messaging.SimpleSerialize.simpleDeserialize;
 import static proto.triton.AiBasicSkills.BasicSkill;
 import static proto.triton.AiIndividualSkills.IndividualSkill;
+import static proto.vision.MessagesRobocupSslDetection.*;
+import static proto.vision.MessagesRobocupSslGeometry.*;
 
 public class IndividualSkillsModule extends Module {
+    SSL_GeometryFieldSize field;
+    HashMap<Integer, SSL_DetectionRobot> allies;
+    HashMap<Integer, SSL_DetectionRobot> foes;
+    ArrayList<SSL_DetectionBall> balls;
+
     public IndividualSkillsModule() throws IOException, TimeoutException {
         super();
     }
@@ -21,12 +31,60 @@ public class IndividualSkillsModule extends Module {
     @Override
     protected void declareExchanges() throws IOException, TimeoutException {
         super.declareExchanges();
-        declareConsume(AI_INDIVIDUAL_SKILL, this::callbackSkill);
+        declareConsume(AI_BIASED_FIELD, this::callbackField);
+        declareConsume(AI_BIASED_ALLIES, this::callbackAllies);
+        declareConsume(AI_BIASED_FOES, this::callbackFoes);
+        declareConsume(AI_BIASED_BALLS, this::callbackBalls);
+        declareConsume(AI_INDIVIDUAL_SKILL, this::callbackIndividualSkill);
 
         declarePublish(AI_BASIC_SKILL);
     }
 
-    private void callbackSkill(String s, Delivery delivery) {
+    private void callbackField(String s, Delivery delivery) {
+        SSL_GeometryFieldSize field;
+        try {
+            field = (SSL_GeometryFieldSize) simpleDeserialize(delivery.getBody());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        this.field = field;
+    }
+
+    private void callbackAllies(String s, Delivery delivery) {
+        HashMap<Integer, SSL_DetectionRobot> allies;
+        try {
+            allies = (HashMap<Integer, SSL_DetectionRobot>) simpleDeserialize(delivery.getBody());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        this.allies = allies;
+    }
+
+    private void callbackFoes(String s, Delivery delivery) {
+        HashMap<Integer, SSL_DetectionRobot> foes;
+        try {
+            foes = (HashMap<Integer, SSL_DetectionRobot>) simpleDeserialize(delivery.getBody());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        this.foes = foes;
+    }
+
+    private void callbackBalls(String s, Delivery delivery) {
+        ArrayList<SSL_DetectionBall> balls;
+        try {
+            balls = (ArrayList<SSL_DetectionBall>) simpleDeserialize(delivery.getBody());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        this.balls = balls;
+    }
+
+    private void callbackIndividualSkill(String s, Delivery delivery) {
         IndividualSkill individualSkill;
         try {
             individualSkill = (IndividualSkill) simpleDeserialize(delivery.getBody());
